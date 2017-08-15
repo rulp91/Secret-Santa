cf0648eeb55e5362c3d0f38f937813707dcecd1c
rperez_movion <rperez@movion.es>
2017-08-15 09:32:33 +0200

    Configuración inicial del entorno https://github.com/rulp91/Secret-Santa/projects/1#card-4219280
     - Creación de un controlador frontal para la aplicación denominado app.php
     - Carga mediante autoload de los espacios de nombres de los vendors
     - Creación de un archivo de descripción de ejecución de la aplicación

---
+++ app.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Implementación de un controlador frontal que servirá de punto de entrada a la aplicación
+ * User: Raul
+ * Date: 15/08/2017
+ * Time: 9:20
+ */
+$loader = require __DIR__.'/vendor/autoload.php';
---
+++ install.md
@@ -0,0 +1,3 @@
+Para ejecutar la aplicación en modo CLI sin tener que implementar porcesos de Docker o de Symfony simplemente hay que teclear la orden php app.php desde linea de comandos.
+
+ 
