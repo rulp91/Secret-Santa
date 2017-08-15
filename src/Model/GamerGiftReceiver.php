e36ec4bb29fa32adb2c7d69b550d20b3da981b1f
rperez_movion <rperez@movion.es>
2017-08-15 10:45:55 +0200

    Creación de un modelo (TAD) para almacenar jugador y recibidor del regalo https://github.com/rulp91/Secret-Santa/issues/5
     - Creación del modelo de amigo invisible con un par de cadenas que simbolizan a la persona que regala y a quien regala
    
     - No soy partidario de constructores parametrizados pero para este juego nos viene bien

---
+++ src/Model/GamerGiftReceiver.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Raul
+ * Date: 15/08/2017
+ * Time: 10:37
+ */
+
+namespace SecretSanta\Model;
+
+class GamerGiftReceiver {
+
+	/**
+	 * @type string
+	 */
+	private $gamer;
+
+	/**
+	 * @type string
+	 */
+	private $giftReceiver;
+
+	/**
+	 * GamerGiftReceiver constructor.
+	 *
+	 * @param string $gamer
+	 * @param string $giftReceiver
+	 */
+	public function __construct($gamer, $giftReceiver) {
+		$this->gamer        = $gamer;
+		$this->giftReceiver = $giftReceiver;
+	}
+
+	/**
+	 * Sobreescribe el toString
+	 */
+	public function __toString() {
+		return $this->gamer . " -> " . $this->giftReceiver;
+	}
+
+}
