fdd8f23008be94833bd7bfaf8f064d371854b59e
rperez_movion <rperez@movion.es>
2017-08-15 09:56:05 +0200

    Creación de un controlador para importar archivos https://github.com/rulp91/Secret-Santa/issues/3
     - Creación de una variable para almacenar el path del fichero

--- src/Controllers/FileLoaderController.php
+++ src/Controllers/FileLoaderController.php
@@ -11,4 +11,59 @@ namespace SecretSanta\Controllers;
 
 class FileLoaderController {
 
+	/**
+	 * @type FileLoaderController
+	 */
+	private static $instance;
+
+	/**
+	 * Indica si la apertura del fichero fue satisfactoria
+	 * @type bool
+	 */
+	private $handleFopenConnection;
+
+	/**
+	 * Almacen de cadenas que almacenrá las lineas leidas
+	 * @type array
+	 */
+	private $linesReaded;
+
+	/**
+	 * Almacenará la ubicación del fichero en el sistema de archivos
+	 * @type string
+	 */
+	private $filePath;
+
+	/**
+	 * FileLoaderController constructor.
+	 */
+	private function __construct() {
+		$this->linesReaded = array();
+	}
+
+	/**
+	 * Acceso al singleton
+	 * @return FileLoaderController
+	 */
+	public static function getInstance() {
+		if (!isset(self::$instance)) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * @param string $filePath
+	 */
+	public function setFilePath(string $filePath) {
+		$this->filePath = $filePath;
+	}
+
+	/**
+	 * Evita que se pueda clonar el Singleton
+	 */
+	public function __clone() {
+		throw new Exception('No se puede clonar un objeto sigleton');
+	}
 }
