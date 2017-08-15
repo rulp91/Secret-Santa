ee4881ca4733b82f3b8f9d2aacd6d166ec1a6e26
rperez_movion <rperez@movion.es>
2017-08-15 11:26:10 +0200

    Creación de un controlador para importar archivos https://github.com/rulp91/Secret-Santa/issues/3
     - Creación de la lógica de selección de amigos invisibles

--- src/Controllers/InvisibleFriendController.php
+++ src/Controllers/InvisibleFriendController.php
@@ -9,6 +9,7 @@
 namespace SecretSanta\Controllers;
 
 use Ds\Set;
+use SecretSanta\Model\GamerGiftReceiver;
 
 class InvisibleFriendController {
 
@@ -32,7 +33,6 @@ class InvisibleFriendController {
 	 * InvisibleFriendController constructor.
 	 */
 	private function __construct() {
-
 	}
 
 	/**
@@ -47,8 +47,52 @@ class InvisibleFriendController {
 		return self::$instance;
 	}
 
+	/**
+	 * Setea los parcipantentes en el amigo invisible
+	 *
+	 * @param array $gamers
+	 */
 	public function setGamers(array $gamers): void {
+		$this->persons                 = new Set($gamers);
+		$this->posibleInvisibleFriends = new Set($gamers);
+	}
 
+	/**
+	 * Retorna una pareja de jugador y amigo invisible o null en caso de que no se haya podido generar dicha pareja
+	 * @return null | GamerGiftReceiver
+	 */
+	public function getNextPairPersonAndGiftReceiver() {
+		$person       = $this->getFriend($this->generate(0, ($this->persons->count() - 1)));
+		$giftReceiver = $this->getGiftReceiver($this->generate(0, ($this->posibleInvisibleFriends->count() - 1)));
+
+		if ($this->isSeleccionable($person, $giftReceiver)) {
+			$this->markSelectedFriend($person);
+			$this->markSelectedGiftReciver($giftReceiver);
+
+			return new GamerGiftReceiver($person, $giftReceiver);
+		}
+
+		return null;
+	}
+
+	/**
+	 * Retorna si hay aún jugadores que no tienen seleccionado amigo invisible
+	 * @return bool
+	 */
+	public function isThereMorePosibilitities() {
+		return !$this->persons->isEmpty();
+	}
+
+	/**
+	 * Metodo sobreescrito que devuelve un random entre dos números
+	 *
+	 * @param int $min
+	 * @param int $max
+	 *
+	 * @return int
+	 */
+	public function generate(int $min, int $max): int {
+		return random_int($min, $max);
 	}
 
 	/**
@@ -56,5 +100,81 @@ class InvisibleFriendController {
 	 */
 	public function __clone() {
 		throw new \Exception('No se puede clonar un objeto sigleton');
+	}
+
+	/**
+	 * Selecciona un jugador random
+	 *
+	 * @param int $i
+	 *
+	 * @return string
+	 */
+	private function getFriend(int $i): string {
+		return $this->selectPosibility($this->persons, $i);
+	}
+
+	/**
+	 * Selecciona un posible recibidor de regalo
+	 *
+	 * @param int $i
+	 *
+	 * @return string
+	 */
+	private function getGiftReceiver(int $i): string {
+		return $this->selectPosibility($this->posibleInvisibleFriends, $i);
+	}
+
+	/**
+	 * Abstraccion de getFriend y getGiftReceiver
+	 *
+	 * @param Set $setPosibilities
+	 * @param int $i
+	 *
+	 * @return string
+	 * @throws \Exception
+	 */
+	private function selectPosibility(Set $setPosibilities, int $i): string {
+		if ($i > $setPosibilities->count() - 1) {
+			throw new \Exception("El índice no es seleccionable ");
+		}
+
+		return $setPosibilities->get($i);
+	}
+
+	/**
+	 * Marca a un jugador como ya seleccionado
+	 *
+	 * @param string $friend
+	 */
+	private function markSelectedFriend(string $friend) {
+		$this->persons->remove($friend);
+	}
+
+	/**
+	 * Marca un amigo invisible como seleccionado
+	 *
+	 * @param string $giftReciver
+	 */
+	private function markSelectedGiftReciver(string $giftReciver) {
+		$this->posibleInvisibleFriends->remove($giftReciver);
+	}
+
+	/**
+	 * Indica si un jugador y un recibidor de regalo son seleccionables
+	 *
+	 * @param $jugador
+	 * @param $posibleGiftReceiver
+	 *
+	 * @return bool
+	 */
+	private function isSeleccionable($person, $posibleGiftReceiver) {
+		if ($this->persons->count() != 2 && $person != $posibleGiftReceiver) {
+			return true;
+		}
+
+		$x = array_values(array_diff($this->persons->toArray(), array($person)));
+		$y = array_values(array_diff($this->posibleInvisibleFriends->toArray(), array($posibleGiftReceiver)));
+
+		return $x[0] != $y[0];
 	}
 }
